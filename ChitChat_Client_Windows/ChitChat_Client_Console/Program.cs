1782ffbe7673cb98c0a7614b1a8505ff07b9b03a
Laurence Luo <admin@lzc.app>
2024-06-27 23:55:47 +0800

    Initial commit

---
+++ ChitChat_Client_Windows/ChitChat_Client_Console/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+namespace ChitChat_Client_Console
+{
+    internal class Program
+    {
+        HubConnection connection;
+
+        static void Main(string[] args)
+        {
+            connection = new HubConnectionBuilder()
+                .WithUrl("http://localhost:53353/ChatHub")
+                .Build();
+        }
+    }
+}
